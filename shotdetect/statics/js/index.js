3dbd128e5dcd8b515433ffa7480bf84dc4c963f4
ZenYinTS <383641115@qq.com>
2021-04-07 02:44:11 +0800

    v1.0

--- shotdetect/statics/js/index.js
+++ shotdetect/statics/js/index.js
@@ -1,7 +1,18 @@
+var myVideo = document.getElementById("myVideo")
+var currentTime = $("#currentTime").val()
+
 $(function () {
     // 搜索内容的显示
     refreshContent()
-    
+
+    //添加视频准备完成后的回调函数
+    if (currentTime!=null){
+        console.log("-------------  开始自动播放  -------------")
+        myVideo.currentTime=parseFloat(currentTime); //跳转
+        myVideo.play();            			//自动播放
+    }
+
+
     //变量定义
     var urlInput = $("#urlInput")
     var divUpload = $("#divUpload")
@@ -38,8 +49,8 @@ function refreshContent(){
 
 $(".m-result").click(function () {
     var formdata = new FormData()
-    console.log($("#resVideo").val())
-    formdata.append('resVal',$("#resVideo").val())
+    // console.log($(this).data("result"))
+    formdata.append('resVal',$(this).data("result"))
     formdata.append('csrfmiddlewaretoken', $('[name=csrfmiddlewaretoken]').val())
 
     $.ajax({
@@ -49,12 +60,12 @@ $(".m-result").click(function () {
         processData: false, //加入这属性    processData默认为true，为true时，提交不会序列化data
         contentType: false,    // 不设置内容类型
         success:function (res) {
-            console.log(res)
-            var myVideo = document.getElementById("myVideo")
+            // console.log(res)
             myVideo.setAttribute("src",res.src)
             //添加视频准备完成后的回调函数
             console.log("-------------  开始自动播放  -------------")
             myVideo.currentTime=res.frame_time;		    //跳转
+            $("#currentTime").val(res.frame_time)
             myVideo.play();            			//自动播放
 
             $("#name").text(res.name)
